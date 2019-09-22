5f02c746163419e04887c76c6ab5e34c381e6704
deciduously <ben@deciduously.com>
2019-09-22 15:15:06 -0400

    Week 4 Part 2

--- Week4/pc2.cpp
+++ Week4/pc2.cpp
@@ -63,29 +63,34 @@ void JokeViewer::displayPunchline()
 {
     using std::cout;
     using std::ios;
+    using std::streampos;
     using std::string;
-    // seek to last character of file
-    punchlineFile.seekg(-1L, ios::end);
-    // if its a newline, seek back again
-    if (punchlineFile.peek() == '\n')
-        punchlineFile.seekg(-1, ios::cur);
 
-    // seek backwards to previous newline
-    char ch;
-    do
-    {
-        punchlineFile.seekg(-1L, ios::cur);
-        punchlineFile.get(ch);
-        cout << ch;
-    } while (ch != '\n');
+    // Seek through file
+    // Store each beginning of line position
+    // When EOF, jump back to latest stored pos
 
-    // output the rest of the file
-    // first, clear clags - EOF got set
-    punchlineFile.clear();
+    // Storage for each beginning of line
+    streampos latestLineBegin = punchlineFile.tellg();
+
+    // Seek to end, locating the last line beginning
     while (!punchlineFile.eof())
     {
         punchlineFile.seekg(1L, ios::cur);
+        char next = punchlineFile.peek();
+        // If we find the end fo the line, overwrite the current line beginning
+        if (next == '\n')
+            latestLineBegin = punchlineFile.tellg() + 1L;
     }
+    // Seek back to stored line beginning
+    punchlineFile.clear();
+    long offset = punchlineFile.tellg() - latestLineBegin;
+    punchlineFile.seekg(-offset, ios::cur);
+
+    // Display line
+    string line;
+    getline(punchlineFile, line);
+    cout << line << "\n";
 }
 
 // Display joke and punchline
